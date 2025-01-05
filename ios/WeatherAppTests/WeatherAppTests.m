a604569a73c8363c8d1e96afd5f5ed7b794e5f03
rehmanprog <rehman.developer@outlook.com>
2025-01-05 23:56:46 +0500

    Application Initialized.

--- ios/WeatherAppTests/WeatherAppTests.m
+++ ios/WeatherAppTests/WeatherAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface WeatherMobileAppTests : XCTestCase
+@interface WeatherAppTests : XCTestCase
 
 @end
 
-@implementation WeatherMobileAppTests
+@implementation WeatherAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
